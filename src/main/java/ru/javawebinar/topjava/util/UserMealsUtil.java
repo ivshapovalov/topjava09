4fd8005293bf21558da88f9451484248a67ed913
Ivan <ivshapovalov@gmail.com>
2016-12-08 10:16:42 +0300

    HW0 - after errors fixing 2

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -7,9 +7,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class UserMealsUtil {
@@ -23,6 +21,33 @@ public class UserMealsUtil {
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 20, 0), "Ужин", 510)
         );
         getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
+    }
+
+    public static List<UserMealWithExceed> getFilteredWithExceededByCycle(List<UserMeal> mealList,
+                                                                    LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
+
+        /***************Решение через циклы*********************/
+
+        Map<LocalDate, Integer> caloriesSumPerDate = new HashMap<>();
+        for (UserMeal meal : mealList
+                ) {
+            caloriesSumPerDate.put(meal.getDate(), caloriesSumPerDate.getOrDefault(meal.getDate(),
+                    0)+ meal.getCalories());
+        }
+
+        List<UserMealWithExceed> userMealWithExceedList = new ArrayList<>();
+        for (UserMeal meal : mealList
+                ) {
+            if (TimeUtil.isBetween(meal.getTime(), startTime, endTime)) {
+                userMealWithExceedList.add(new
+                        UserMealWithExceed(meal
+                        .getDateTime(),
+                        meal.getDescription(), meal.getCalories(),
+                        caloriesSumPerDate.get(meal.getDate()) > caloriesPerDay));
+            }
+
+        }
+        return userMealWithExceedList;
     }
 
     public static List<UserMealWithExceed> getFilteredWithExceeded(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
