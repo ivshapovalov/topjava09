388610857f5f6f09ad03e96f80ac6a14155cc00b
Ivan <ivshapovalov@gmail.com>
2016-12-28 10:12:31 +0300

    HW3 inMemory tests

--- src/test/java/ru/javawebinar/topjava/web/InMemoryAdminRestControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/InMemoryAdminRestControllerTest.java
@@ -21,7 +21,7 @@ public class InMemoryAdminRestControllerTest {
 
     @BeforeClass
     public static void beforeClass() {
-        appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml");
+        appCtx = new ClassPathXmlApplicationContext("spring/spring-test.xml");
         System.out.println("\n" + Arrays.toString(appCtx.getBeanDefinitionNames()) + "\n");
         controller = appCtx.getBean(AdminRestController.class);
     }
@@ -30,8 +30,6 @@ public class InMemoryAdminRestControllerTest {
     public static void afterClass() {
         appCtx.close();
     }
-
-
 
     @Before
     public void setUp() throws Exception {
