7afe2d0c5c3e5e435fb5034e4929ed182f35a4ee
Ivan <ivshapovalov@gmail.com>
2016-12-28 09:41:53 +0300

    HW2 3

--- src/main/java/ru/javawebinar/topjava/repository/InMemoryMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/InMemoryMealRepositoryImpl.java
@@ -8,10 +8,6 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
-/**
- * GKislin
- * 15.09.2015.
- */
 public class InMemoryMealRepositoryImpl implements MealRepository {
     private Map<Integer, Meal> repository = new ConcurrentHashMap<>();
     private AtomicInteger counter = new AtomicInteger(0);
